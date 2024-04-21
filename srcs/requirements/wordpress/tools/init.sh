a03cc26ddc2c36d3b96022ace6c5d2d4eb389223
exiled <exiled@Ubuntu.exiled.42.fr>
2024-04-21 17:01:39 +0100

    mandatory

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 5
+
 WP_CONFIG="/var/www/html/wp-config.php"
 
 
@@ -9,6 +11,13 @@ sed -i "s/'username_here'/'$MYSQL_USER'/g" "$WP_CONFIG"
 sed -i "s/'password_here'/'$MYSQL_PASSWORD'/g" "$WP_CONFIG"
 sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306');/" "$WP_CONFIG"
 
+wp core install --allow-root \
+    --url=localhost \
+    --title="Inception" \
+    --admin_user=$WP_ADMIN \
+    --admin_password=$WP_ADMIN_PASSWORD \
+    --admin_email="olahrizi@student.1337.ma";
 
+wp user create --allow-root $WP_USER "flyeroussama@gmail.com" --user_pass=$WP_USER_PASSWORD;
 
-bash
+exec $@
