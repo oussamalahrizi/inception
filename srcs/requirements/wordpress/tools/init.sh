6db1bd49ee44da71b59fc0ef05661bc9e81dbca3
oussamalahrizi <flyeroussama@gmail.com>
2024-04-23 18:34:26 +0100

    2

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,12 +4,14 @@ sleep 5
 
 WP_CONFIG="/var/www/html/wp-config.php"
 
-
+host="https://192.168.0.101"
 
 sed -i "s/'database_name_here'/'$MYSQL_DATABASE'/g" "$WP_CONFIG"
 sed -i "s/'username_here'/'$MYSQL_USER'/g" "$WP_CONFIG"
 sed -i "s/'password_here'/'$MYSQL_PASSWORD'/g" "$WP_CONFIG"
 sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306');/" "$WP_CONFIG"
+
+
 
 wp core install --allow-root \
     --url=localhost \
@@ -19,5 +21,10 @@ wp core install --allow-root \
     --admin_email="olahrizi@student.1337.ma";
 
 wp user create --allow-root $WP_USER "flyeroussama@gmail.com" --user_pass=$WP_USER_PASSWORD;
+
+wp theme install twentytwentytwo --activate --allow-root
+
+wp plugin update --all --allow-root
+
 
 exec $@
