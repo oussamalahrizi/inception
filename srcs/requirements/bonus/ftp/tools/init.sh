fc6b9609b6dd12eee13ea481aa160bcc50b1bf25
exiled <exiled@Ubuntu.exiled.42.fr>
2024-04-25 18:06:24 +0100

    ftp done

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -4,7 +4,7 @@
 mkdir -p /var/www/html
 
 
-adduser $FTP_USER --disabled-password
+adduser  --disabled-password --gecos "" "$FTP_USER"
 echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd &> /dev/null
 chown -R $FTP_USER:$FTP_USER /var/www/html
 
